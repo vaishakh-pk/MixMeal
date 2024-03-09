7ecdcb73021671dd5387f81b7cd254175c226db6
VK <vaishakh.aravind@gmail.com>
2024-03-09 20:12:20 +0530

    initial

---
+++ android/app/src/main/kotlin/com/example/mixmeal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mixmeal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/categories.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mixmeal/data/dummy_data.dart';
+import 'package:mixmeal/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var kcolour = Colors.white;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: [
+            for(final category in availableCategories)
+              CategoryGridItem(category: category)
+          ]),
+    );
+  }
+}
